733b8436c149c5a689a71c2bd4a1d2db7e535628
zhuyanglei <yanglei.zhu@mljr.com>
2017-07-25 17:30:35 +0800

    第一次提交

---
+++ TestCocoaPod/TestCocoaPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCocoaPod
+//
+//  Created by zylkdd on 2017/7/25.
+//  Copyright © 2017年 zylkdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCocoaPod/TestCocoaPod/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestCocoaPod
+//
+//  Created by zylkdd on 2017/7/25.
+//  Copyright © 2017年 zylkdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
